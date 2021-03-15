36d210f3e09a513dc9b8237d09ce98c4ee1b87ed
GooGee <mou-gj@live.com>
2021-03-15 16:09:00 +0800

    + bootstrap.min.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/css/bootstrap.min.css'
+import './assets/css/style.css'
 
 createApp(App)
-  .use(router)
-  .mount("#app");
+    .use(router)
+    .mount('#app')
