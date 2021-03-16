338d88f97a4dec6c662e72b90f2eb8ca8df1bc74
GooGee <mou-gj@live.com>
2021-03-16 17:19:54 +0800

    + SearchView.vue

---
+++ src/view/part/SearchView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-2 col-xl-3"></div>
+        <div class="col-8 col-xl-6">
+            <input v-model="text" type="text" class="form-control" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SearchStore from '@/store/SearchStore'
+
+export default defineComponent({
+    setup() {
+        return SearchStore
+    },
+})
+</script>
