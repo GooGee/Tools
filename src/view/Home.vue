2a78c663537ce4f57221f91c5d32cf9d1fcbf8f0
GooGee <mou-gj@live.com>
2021-03-16 17:20:11 +0800

    + ToolList in Home.vue

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,8 +1,27 @@
 <template>
     <div>
-        <div class="text-center" style="margin-top: 11%">
-            <img alt="logo" src="../assets/image/logo.svg" style="width: 222px" />
+        <div class="text-center">
             <h1>Dev Tools</h1>
         </div>
+
+        <SearchView class="mtb11"></SearchView>
+
+        <ToolList></ToolList>
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SearchView from './part/SearchView.vue'
+import ToolList from './part/ToolList.vue'
+
+export default defineComponent({
+    components: {
+        SearchView,
+        ToolList,
+    },
+    setup() {
+        return {}
+    },
+})
+</script>
