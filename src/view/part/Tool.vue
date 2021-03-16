09cf58ada30571ad3d212c8f29b0062ab548653f
GooGee <mou-gj@live.com>
2021-03-16 17:19:37 +0800

    + Tool.vue

---
+++ src/view/part/Tool.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="list-group-item">
+        <div>
+            <a :href="tool.url" target="_blank">{{ tool.title }}</a>
+        </div>
+        <div>{{ tool.description }}</div>
+    </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        tool: {
+            type: Object,
+            required: true,
+        },
+    },
+    setup() {
+        return {}
+    },
+})
+</script>
