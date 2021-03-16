b785e48a3540627fd3937e21c2f9d886d38fd040
GooGee <mou-gj@live.com>
2021-03-16 17:19:14 +0800

    + SourceStore.ts

---
+++ src/store/SourceStore.ts
@@ -0,0 +1,39 @@
+import Source from '@/model/Source'
+import MiniSearch from 'minisearch'
+import { ref } from 'vue'
+import RequestStore from './RequestStore'
+
+const state = {
+    source: new Source(),
+}
+
+const ready = ref(false)
+
+const ms = new MiniSearch({
+    fields: ['title', 'description'],
+    storeFields: ['title', 'description', 'url', 'id'],
+    searchOptions: {
+        boost: {
+            title: 2,
+        },
+        prefix: true,
+        fuzzy: 0.3,
+    },
+})
+
+RequestStore.getSource()
+    .then(response => response.json())
+    .then(data => {
+        ms.addAll(data.toolxx)
+        state.source = data
+        ready.value = true
+    })
+
+function filter(text: string) {
+    return ms.search(text)
+}
+
+export default {
+    ready,
+    filter,
+}
