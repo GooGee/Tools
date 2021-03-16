a6fc128f484f8714edf902ab5022d3f25e022f30
GooGee <mou-gj@live.com>
2021-03-16 15:16:53 +0800

    - HelloWorld.vue

---
+++ src/view/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <div class="text-center" style="margin-top: 11%">
+            <img alt="logo" src="../assets/image/logo.svg" style="width: 222px" />
+            <h1>Dev Tools</h1>
+        </div>
+    </div>
+</template>
